f6ccea5684c2674182a6f72c57a817754d9355c7
derderdevil <dwarden@mail.uni-mannheim.de>
2020-02-02 17:30:55 +0100

    import more stuff for rest

--- src/main/java/rest/GreetingController.java
+++ src/main/java/rest/GreetingController.java
@@ -4,24 +4,28 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicLong;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-
-
-@RestController
+@Controller
 public class GreetingController {
 
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
+
     Map<String, Integer> map = new HashMap<>();
 
-    @GetMapping("/greeting")
+
+
+   /* @GetMapping("/greeting")
     public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
         return new Greeting(counter.incrementAndGet(), String.format(template, name));
-    }
+    }*/
 
     @RequestMapping("/person")
     public void enterPerson(@RequestParam(value = "name") String name, @RequestParam(value ="age") int age){
@@ -39,6 +43,14 @@ public class GreetingController {
         }
         return persons;
     }
+
+
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
+    }
+
 
 
 }
